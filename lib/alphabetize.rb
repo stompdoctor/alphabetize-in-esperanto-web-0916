eab91f17a7a012bff2d14098a4874cdfe3913a0e
stompdoctor <jonjpeterkin@gmail.com>
2016-09-23 16:51:46 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
-def alphabetize(arr)
-  # code here
+require "pry"
+
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(ary)
+	mag = {}
+  ary.each do |phrase| 
+  	new_phrase = ""
+  	phrase.each_char { |chr| new_phrase << ALPHABET.index(chr) if ALPHABET.include?(chr)}
+  	mag[phrase] = new_phrase
+  end
+  ary.sort_by { |phrase| mag[phrase] }
 end
